c46743fb938c13931e003815091501e601a92da1
extercontrol <60764890+extercontrol@users.noreply.github.com>
2021-12-20 16:39:33 -0600

    Add files via upload

---
+++ validadorDocs/script.js
@@ -0,0 +1,29 @@
+function getUrlVars() {
+		var vars = {};
+		var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+		vars[key] = value;
+	});
+	return vars;
+}
+
+var hash = getUrlVars()['hash'];
+
+var url = "https://script.google.com/macros/s/AKfycbylemaWUr4C5rLhYYICaXOk3deSzaHQjLmle8eBnri2JbxYYw3AnsgxPheJowx9zI2N/exec?callback=ctrlq&id="+hash;
+
+$.getJSON(url, function (data) {
+	
+	if(data.encontrado === "True"){
+		$('#hash').text("hash: "+data.hash);
+		$('#tipodoc-v').text(data.tipodoc);
+		$('#informacion-v').text(data.informacion);
+		$('#validez-v').text(data.validez);
+	}
+	else{
+		$('#info-valido').toggleClass('invisible');
+		$('#error').toggleClass('invisible');
+	}
+	$('#loader').css("opacity","0.0");
+	$('#loader').one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",function(event) {
+	    $('#loader').css("display","none");
+	});
+});
